73399c98d95793cbe161b052684246a1cb610dd1
Mo <folarinfadeyi@gmail.com>
2023-07-11 21:18:58 -0700

    minimal changes here and there

---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <form>
+            <input placeholder='Add To-Dos...' />
+            <button>Add To-Do!</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form;
