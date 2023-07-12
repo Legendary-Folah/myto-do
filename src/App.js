02fbee9b2a28d05d7891ed62869d986fe07b40e4
Mo <folarinfadeyi@gmail.com>
2023-07-11 21:20:02 -0700

    minimal changes here and there

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Form from './Components/Form';
 import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 }
