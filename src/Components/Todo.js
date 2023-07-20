dddf165ee349b86473a1167752d0560895809be4
Mo <folarinfadeyi@gmail.com>
2023-07-20 12:10:50 -0700

    minimal changes here and there

---
+++ src/Components/Todo.js
@@ -0,0 +1,12 @@
+const Todo = () => {
+    return (
+        <div>
+            <div>
+                <h3>Todo Item</h3>
+                <button>Del</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
