f4be40d2f1a9324f0de9550c888ce66adbcdd630
Mo <folarinfadeyi@gmail.com>
2023-07-20 12:49:54 -0700

    minimal changes here and there

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,11 @@
+import { faNoteSticky } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Header = () => {
     return (
         <div className="header">
             My To-Do!
+            <FontAwesomeIcon icon={faNoteSticky} />
         </div>
     )
 }
