c77ba26512415c48ffa62c51a7f3c3645b6b245a
Mo <folarinfadeyi@gmail.com>
2023-07-20 11:22:47 -0700

    minimal changes here and there

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './Components/Form';
 import Header from './Components/Header';
 
 function App() {
+
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodoList] = useState([])
+
   return (
     <div className="App">
       <Header />
