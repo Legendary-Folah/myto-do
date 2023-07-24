af4f769b9640e91d91579c438974e0f1e0756dfa
Mo <folarinfadeyi@gmail.com>
2023-07-24 11:10:13 -0700

    minimal changes yet again

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,9 +5,20 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Todo = ({ todoItem }) => {
 
+    // const completedStyle = {
+    //     fontStyle: "Italic",
+    //     textDecoration: "line-through",
+    //     color: "rgb(100, 105, 110)",
+    // };
+
+    // const handleClick = (event) => {
+    //     event.target.style = completedStyle;
+    // }
+
     return (
         <div>
             <div className="todoItem">
+                <input type="checkbox" className="check"/>
                 <h3 className="todoname">{todoItem}</h3>
                 <FontAwesomeIcon icon={faTrashAlt} className="deletebutton"/>
             </div>
